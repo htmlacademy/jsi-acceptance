5d27c97afdad081782d6ae1ef7f038a332ea92b7
Tangerine Cat <kaineer@gmail.com>
2016-11-01 10:29:44 +0500

    Canvas для Кекстаграмм

---
+++ test/mock-context-test.js
@@ -0,0 +1,74 @@
+// test/mock-context-test.js
+
+var expect = require('chai').expect;
+var MockContext = require('..').MockContext;
+
+var Canvas2dMethods = [
+  // Rectangles methods
+  'clearRect', 'fillRect', 'strokeRect',
+
+  // Drawing text methods
+  'fillText', 'strokeText', 'measureText',
+
+  // Paths methods
+  'beginPath', 'closePath', 'moveTo', 'lineTo', 'arc', 'ellipse', 'rect',
+
+  // Drawing paths methods
+  'fill', 'stroke', 'clip', 'isPointInPath', 'isPointInStroke',
+
+  // Transform methods
+  'rotate', 'scale', 'transform', 'setTransform', 'resetTransform',
+
+  // Image methods
+  'drawImage',
+
+  // Canvas state methods
+  'save', 'restore'
+];
+
+describe('MockContext', function() {
+  var ctx;
+
+  beforeEach(function() {
+    ctx = new MockContext();
+  });
+
+  it('should be ok', function() {
+    expect(ctx).to.be.ok;
+  });
+
+  Canvas2dMethods.forEach(function(method) {
+    it('should respond to ' + method, function() {
+      expect(ctx).to.respondTo(method);
+    });
+  });
+
+  context('after called some methods', function() {
+    beforeEach(function() {
+      ctx.beginPath();
+      ctx.rect(0, 0, 100, 100);
+      ctx.closePath();
+    });
+
+    it('should report that methods have been called', function() {
+      expect(ctx.hasCalled('beginPath')).to.be.ok;
+      expect(ctx.hasCalled('rect')).to.be.ok;
+      expect(ctx.hasCalled('closePath')).to.be.ok;
+    });
+
+    it('should report arguments', function() {
+      expect(ctx.hasCalled('beginPath').with()).to.be.ok;
+      expect(ctx.hasCalled('rect').with(0, 0, 100, 100)).to.be.ok;
+    });
+
+    it('should report arguments to callback', function() {
+      expect(ctx.hasCalled('rect').with(function(x, y, width, height) {
+        return width === 100 && height === 100;
+      })).to.be.ok;
+    });
+
+    it('should report sequence', function() {
+      expect(ctx.hasSequence('beginPath', 'closePath')).to.be.ok;
+    });
+  });
+});
