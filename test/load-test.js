87c8b6856afef6cf2bc9e6381900ba0b97685ea3
Tangerine Cat <kaineer@gmail.com>
2017-01-18 14:17:42 +0500

    Добавил возможность перекрывать значения переменных в load

--- test/load-test.js
+++ test/load-test.js
@@ -4,12 +4,13 @@ var path = require('path');
 var expect = require('chai').expect;
 var load = require('../lib/load');
 
-var loadFixture = function(name) {
+var loadFixture = function(name, overrides) {
   return load(
     path.resolve(
       path.join(__dirname, 'fixtures/load', name + '.js')
     ),
-    ['hello']
+    ['hello', 'number'],
+    overrides
   );
 };
 
@@ -27,5 +28,17 @@ describe('load module', function() {
   it('should load function from function-file', function() {
     var obj = loadFixture('hello-function');
     expect(obj.hello).to.be.a('function');
+  });
+
+  context('load with/without overrides', function() {
+    it('should load number from hello-var', function() {
+      var obj = loadFixture('hello-var');
+      expect(obj.number).to.eq(38);
+    });
+
+    it('should load overridden number from hello-var', function() {
+      var obj = loadFixture('hello-var', {number: 42});
+      expect(obj.number).to.eq(42);
+    });
   });
 });
